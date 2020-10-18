70d7678eaf9df73cda5c1d62acd0c1d8215e0d4f
evalbors <evalbors@gmail.com>
2020-10-18 16:35:47 +0200

    play works with 2 players: they can score and hold the score but when dice=1, score is 0 and change player

---
+++ app.js
@@ -0,0 +1,79 @@
+var scores, roundScore, activePlayer;
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+// hide dice at the beggining: changing into css property
+document.querySelector('.dice').style.display = 'none';
+
+// EVENT HANDLER
+// addEventListener('first put the event type', 'second, the funtion we need as soon this event happens -without ()-, the callback function')
+// callback, call a fun inside another fun
+// anonimus fun will be put function() { //do st here} - without name - 
+// It's perfect if ypu only use this function only once, like our dice
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+// ROLL DICE
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    // 1. random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. display result
+    var diceDOM =  document.querySelector('.dice')
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'images/dice-' + dice + '.png';
+    
+    // 3. updte the round score if the rolled number is not a 1
+
+    if (dice == 1 ){
+        roundScore = 0;
+        nextPlayer();
+    }
+
+    if (dice !== 1 ){
+        roundScore += dice;
+        // in other code: roundScore = roundScore + dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
+});
+
+// HOLD SCORE
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    scores[activePlayer] += roundScore;
+    // in other code: score[activePlayer] = scores[activePlayer] + roundScore
+
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    // check if score => 100
+
+    nextPlayer();
+});
+ 
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        // current score into red square
+        document.getElementById('current-0').textContent ='0';
+        document.getElementById('current-1').textContent ='0';
+
+        // move active state in the another player - VERSION 2 -
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        // hide the dice
+        document.querySelector('.dice').style.display = 'none';
+}
+
+
+// SETTER
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+// document.querySelector('#current-' + activePlayer).innerHTML = '<strong>' + dice + '</strong>';
+
+// GETTER
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
