1639a6dfe3938d89e8cdcf4a09fd51bbe4061417
evalbors <evalbors@gmail.com>
2020-10-18 21:20:22 +0200

    trying before if player gets 20 to check the code. Now, It's correct: UI change when a player wins

--- app.js
+++ app.js
@@ -49,13 +49,13 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     
     // check if score => 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
         document.querySelector('.dice').style.display = 'none';
     }
-    
+
     if (scores[activePlayer] < 20) {
         nextPlayer();
     }
